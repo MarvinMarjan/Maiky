ff4d07cb2465badb926068039c999fc257125c7e
Marvin Marjan <marvin.marjanc@hotmail.com>
2022-06-19 10:32:15 -0400

    remove maiky_repository

--- Maiky/maiky_repository/source/bootstrap.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Bootstrap
-{
-public:
-	static std::map<std::string, std::string> check_runtime_config();
-};
--- Maiky/maiky_repository/source/exception.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Exception
-{
-public:
-	static void _initial_code_block_not_detected(std::string lines, int highlight);
-};
